57aa3a3de7fc0eac900cd3a068a5f7070703dac9
husnu.tazkiya@jiarsi.com <husnu.tazkiya@jiarsi.com>
2025-06-12 18:16:03 +0700

    fixed router

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/webportal/';
+$config['base_url'] = 'http://localhost/web-portal/';
 
 $config['index_page'] = '';
 
